2e4dccac90514b20b51fb93d571fdce8f6b7d3c5
BenjaminSlijper <bcslijper@gmail.com>
2025-03-15 12:08:13 +0100

    Added DataFcn stub for ltvss model

---
+++ ltvStockModel.m
@@ -0,0 +1,12 @@
+function [A,B,C,D,E,dx0,x0,u0,y0,Delays] = ltvStockModel(k)
+    A = [];
+    B = [];
+    C = [];
+    D = [];
+    E = [];
+    dx0 = [];
+    x0 = [];
+    u0 = [];
+    y0 = [];
+    Delays = [];
+end
