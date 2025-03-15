d2426340d7b6a82efc9a0a6fee8fd6abf8db6ee5
BenjaminSlijper <bcslijper@gmail.com>
2025-03-15 13:31:18 +0100

    made model

--- ltvStockModel.m
+++ ltvStockModel.m
@@ -1,7 +1,43 @@
 function [A,B,C,D,E,dx0,x0,u0,y0,Delays] = ltvStockModel(k)
-    A = [];
-    B = [];
-    C = [];
+
+    apple = readmatrix("data_processed\apple_return.csv");
+    boeing = readmatrix("data_processed\boeing_return.csv");
+    nvidia = readmatrix("data_processed\nvidia_return.csv");
+    oneok = readmatrix(	"data_processed\oneok_return.csv");
+    
+    bank_return = 0.01; % store somewhere else
+    alpha = 0.0001;  % store somewhere else
+    beta = 0.0001;  % store somewhere else
+    
+    apple_return = apple(:, 2);
+    boeing_return = boeing(:, 2);
+    nvidia_return = nvidia(:, 2);
+    oneok_return = oneok(:, 2);
+    
+    A = diag([1 + apple_return(k); ...
+              1 + boeing_return(k); ...
+              1 + nvidia_return(k); ...
+              1 + oneok_return(k); ...
+              1 + bank_return]);
+    
+    return_matrix = diag([1 + apple_return(k); ...
+              1 + boeing_return(k); ...
+              1 + nvidia_return(k); ...
+              1 + oneok_return(k)]);
+    
+    final_row_B_1 = ones(1, length(return_matrix)) * (1 + bank_return) * (-1 - alpha);
+    final_row_B_2 = ones(1, length(return_matrix)) * (1 + bank_return) * (1 + beta);
+    
+    B = [return_matrix -return_matrix;
+        final_row_B_1 final_row_B_2];
+    B_final_col = zeros(length(B(:, 1)), 1);
+    B_final_col(end) = 1 + bank_return;
+    
+    B = [B B_final_col];
+    
+    C = ones(1, length(A));
+    C(end) = -1;
+
     D = [];
     E = [];
     dx0 = [];
