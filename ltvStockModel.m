b3505345374c8e86bde641079c1d1a465a131cb8
BenjaminSlijper <bcslijper@gmail.com>
2025-03-18 19:04:29 +0100

    make csv data persistent

--- ltvStockModel.m
+++ ltvStockModel.m
@@ -1,13 +1,19 @@
 function [A,B,C,D,E,dx0,x0,u0,y0,Delays] = ltvStockModel(k)
 
-    apple = readmatrix("data_processed\apple_return.csv");
-    boeing = readmatrix("data_processed\boeing_return.csv");
-    nvidia = readmatrix("data_processed\nvidia_return.csv");
-    oneok = readmatrix(	"data_processed\oneok_return.csv");
+    % Declare persistent variables
+    persistent apple boeing nvidia oneok;
     
-    bank_return = 0.1; % store somewhere else
-    alpha = 0.0001;  % store somewhere else
-    beta = 0.0001;  % store somewhere else
+    % If data is not yet loaded, read from CSV files
+    if isempty(apple) || isempty(boeing) || isempty(nvidia) || isempty(oneok)
+        apple = readmatrix("data_processed\apple_return.csv");
+        boeing = readmatrix("data_processed\boeing_return.csv");
+        nvidia = readmatrix("data_processed\nvidia_return.csv");
+        oneok = readmatrix("data_processed\oneok_return.csv");
+    end
+    
+    interest = 0.1; 
+    alpha = 0.0001; 
+    beta = 0.0001;
     
     apple_return = apple(:, 2);
     boeing_return = boeing(:, 2);
@@ -18,23 +24,23 @@ function [A,B,C,D,E,dx0,x0,u0,y0,Delays] = ltvStockModel(k)
               1 + boeing_return(k+1); ...
               1 + nvidia_return(k+1); ...
               1 + oneok_return(k+1); ...
-              1 + bank_return]);
+              1 + interest]);
     
     return_matrix = diag([1 + apple_return(k+1); ...
               1 + boeing_return(k+1); ...
               1 + nvidia_return(k+1); ...
               1 + oneok_return(k+1)]);
     
-    final_row_B_1 = ones(1, length(return_matrix)) * (1 + bank_return) * (-1 - alpha);
-    final_row_B_2 = ones(1, length(return_matrix)) * (1 + bank_return) * (1 - beta);
+    final_row_B_1 = ones(1, length(return_matrix)) * (1 + interest) * (-1 - alpha);
+    final_row_B_2 = ones(1, length(return_matrix)) * (1 + interest) * (1 - beta);
     
     B = [return_matrix -return_matrix;
         final_row_B_1 final_row_B_2];
     
     C = ones(1, length(A));
-    % C(end) = -1;
 
     D = zeros(1, length(B(1, :)));
+    
     E = [];
     dx0 = [];
     x0 = [];
--- main.m
+++ main.m
@@ -1,9 +1,14 @@
-system = ltvss(@ltvStockModel, 1);
+dT = 1;
 
-t = 0:11;
+system = ltvss(@ltvStockModel, "Ts", dT, "TimeUnit", 'days');
+
+t = 0:dT:11;
 x0 = [0 0 0 0 10000]';
-u = [ones(12,4)*100, zeros(12, 4)];
+u = [ones(5,4)*1000, zeros(5, 4) ; zeros(7, 8)];
 
-[y, ~, x] = lsim(system, u, t, x0);
-
+figure()
+lsim(system, u, t, x0)
+hold on
+[y,t,x,p] = lsim(system, u, t, x0);
 plot(t, x)
+hold off
