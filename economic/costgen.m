020657d1dac3f35d017d5879d5accaf5ef81fd1b
BenjaminSlijper <bcslijper@gmail.com>
2025-04-09 16:17:42 +0200

    reorganized files into folders and added economic files (copies of tracking)

---
+++ economic/costgen.m
@@ -0,0 +1,15 @@
+function [H,h,const]=costgen(T,S,Q,R,P,dim,x0,xr,ur)
+
+% Change to make suitable for economic MPC
+
+Qbar = blkdiag(kron(eye(dim.N - 1),Q),P); 
+Rbar = kron(eye(dim.N),R);
+H = S'*Qbar*S+Rbar;
+
+hx0 = S'*Qbar*T*x0;
+hxr = -S'*Qbar*kron(ones(dim.N,1),eye(dim.nx))*xr;
+hur = -Rbar*kron(ones(dim.N,1),eye(dim.nu))*ur;
+h = hx0 + hxr + hur;
+const=x0'*T'*Qbar*T*x0;
+
+end
---
+++ tracking/tracking_MPC.m
@@ -0,0 +1,86 @@
+yalmip('clear')
+
+%% Variables
+
+%Definition of system dimension
+dim.nx=5;     %state dimension
+dim.ny=1;     %output dimension
+dim.nu=8;     %input dimension
+dim.N=20;      %horizon
+
+Nsim = 200;  %simulation time
+
+x0 = [0, 0, 0, 0, 100]';  %initial condition
+y_ref = 10000;  %TODO find optimal y_ref in the MPC loop
+
+Q = 50*eye(dim.nx, dim.nx);
+R = eye(dim.nu, dim.nu);
+
+alpha = 0.0001;
+beta = 0.0001;
+
+% --- Compute covariance matrix and return matrix
+total_return_matrix = zeros(1200, dim.nx);
+for k=1:1200
+    [~, return_matrix] = ltvStockModel(k);
+    total_return_matrix(k, :) = diag(return_matrix);
+end
+sigma = cov(total_return_matrix);
+avg_returns = mean(total_return_matrix, 1)';
+
+%% MPC
+
+% Setting up variables
+x = zeros(dim.nx,Nsim);  %state trajectory
+x(:,1)=x0;
+y = zeros(1, Nsim);  %output trajectory
+y(1) = sum(x0);
+
+uostar = zeros(dim.nu*dim.N, 1);
+opts = optimoptions('quadprog','Algorithm','active-set', 'Display', 'off');
+ws = optimwarmstart(uostar, opts);
+
+u_rec=zeros(dim.nu,Nsim+1);  %optimal inputs over time
+
+for k=1:Nsim
+    disp(k)
+    
+    % Setting up matrices
+    [T,S] = predmodgen_ltv(@ltvStockModel, k, dim);
+    [A_ineq,b_ineq, F,E,e] = constraintgen(4, alpha, beta, T, S, dim, x(:, k));
+
+    [A_N,B_N,C_N] = ltvStockModel(k+dim.N);  %state matrices at end of prediction horizon
+    [xr,ur] = solve_ots(A_N, B_N, C_N, avg_returns, sigma, y_ref, F, E, e, dim);
+
+    [P,~] = idare(A_N, B_N, Q, R);
+    [H,h,const] = costgen(T, S, Q, R, P, dim, x(:, k), xr, ur);
+
+    %Solve optimization problem    
+    warning off all
+    options = optimoptions('quadprog', 'Display', 'off');
+    [ws,fval,eflag,output,lambda] = quadprog(H, h, A_ineq, b_ineq, [], [], [], [], ws);
+    uostar = ws.X;
+    warning on all
+
+    % Select the first input only
+    u_rec(:,k) = uostar(1:dim.nu);
+
+    % Compute the state/output evolution
+    [A,B,C,~] = ltvStockModel(k);
+    x(:,k+1) = A*x(:,k) + B*u_rec(:,k);
+    y(k+1) = C*x(:,k+1);
+end
+
+%% Plotting
+
+for i=1:5
+    figure(i);
+    plot(0:Nsim, x(i, :))
+    title((sprintf("Stock %i", i)))
+end
+figure(6);
+plot(0:Nsim, y)
+hold on
+yline(y_ref, "Color", 'black')
+hold off
+title("Total portfolio")
